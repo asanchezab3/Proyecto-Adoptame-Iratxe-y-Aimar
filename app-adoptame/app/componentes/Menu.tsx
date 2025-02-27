1f99795147d5605a9407e58fbd41cf4e41594189
Iratxe <iremoncdam1@gmail.com>
2025-02-27 20:47:39 +0100

    Mejorado apartado responsive del menú superior

--- app-adoptame/app/componentes/Menu.tsx
+++ app-adoptame/app/componentes/Menu.tsx
@@ -7,23 +7,23 @@ const Menu = () => {
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@100..700"/>
         <div className="flex items-center space-x-10">
             <Link to="/">
-                <img src="/images/logo_bg.png" alt="Logo" className="ml-6 h-12" />
+                <img src="/images/logo_bg.png" alt="Logo" className="ml-2 lg:ml-10 h-8 lg:h-12" />
             </Link>
             
             <h1 className="text-sm sm:text-base md:text-lg font-bold hidden md:block">VIDA CON PATITAS</h1>
         </div>
         
-        <ul className="flex space-x-4 sm:space-x-12 md:space-x-24 mr-8">
+        <ul className="flex space-x-2 sm:space-x-12 md:space-x-24 mr-2 md:mr-8">
             <li className="flex items-center">
-                <span className="material-symbols-outlined mr-4">pets</span>
+                <span className="material-symbols-outlined mr-2 lg:mr-4">pets</span>
                 <Link className="text-sm sm:text-base md:text-lg" to="/lista">Animales</Link>
             </li>
             <li className="flex items-center">
-                <span className="material-symbols-outlined mr-4">person</span>
-                <p className="text-sm sm:text-base md:text-lg">Iniciar sesión</p>
+                <span className="material-symbols-outlined mr-2 lg:mr-4">person</span>
+                <Link className="text-sm sm:text-base md:text-lg" to="">Iniciar sesión</Link>
             </li>
             <li className="flex items-center">
-                <span className="material-symbols-outlined mr-4">mail</span>
+                <span className="material-symbols-outlined mr-2 lg:mr-4">mail</span>
                 <Link className="text-sm sm:text-base md:text-lg" to="/contacto">Contacto</Link>
             </li>
         </ul>
