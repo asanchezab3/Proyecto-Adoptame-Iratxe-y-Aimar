25322ea2565e1fac2b2b6f5e54024a60bba253ca
Iratxe <iremoncdam1@gmail.com>
2025-03-09 23:08:42 +0100

    Añadida funcionalidad de borrar campos y mostrar mensaje al enviar formulario de contacto

--- app-adoptame/app/routes/contacto.tsx
+++ app-adoptame/app/routes/contacto.tsx
@@ -1,6 +1,32 @@
 import Menu from "../components/Menu";
+import { useEffect, useState } from "react";
 
 const Contacto = () => {
+
+
+    const [mensajeEnviado, setMensajeEnviado] = useState(false);
+
+    const manejarEnvio = (e: React.FormEvent) => {
+        {/* Al enviarse el formulario se borra el texto de todos los campos*/}
+        const nombre = document.getElementById("nombre") as HTMLInputElement;
+        if (nombre) nombre.value = "";
+        const correo = document.getElementById("correo") as HTMLInputElement;
+        if (correo) correo.value = "";
+        const asunto = document.getElementById("asunto") as HTMLInputElement;
+        if (asunto) asunto.value = "";
+        const mensaje = document.getElementById("mensaje") as HTMLInputElement;
+        if (mensaje) mensaje.value = "";
+
+        {/* Se muestra el mensaje de envio de formulario correcto durante tres segundos*/}
+        e.preventDefault(); 
+        setMensajeEnviado(true);
+
+        setTimeout(() => {
+            setMensajeEnviado(false);
+        }, 3000); 
+    };
+
+
     return (
         <div className="min-h-screen bg-white">
 
@@ -11,25 +37,34 @@ const Contacto = () => {
                 {/* Formulario */}
                 <section className="mt-10 mb-10 ml-10 lg:ml-40 mr-10 lg:mr-20">
                     <h2 className="text-sm lg:text-lg mb-10 text-center">¡Rellena nuestro formulario de contacto!</h2>
-                    <form className="bg-[#dbcfcb] min-h-100 lg:min-h-170 rounded-lg grid grid-rows-11 p-4 lg:p-10">
+
+
+                    {/* Mensaje de formulario enviado */}
+                    {mensajeEnviado && (
+                        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-green-600 text-white px-6 rounded-lg text-lg font-semibold min-h-30 flex items-center">
+                            Mensaje enviado correctamente
+                        </div>
+                    )}
+
+                    <form className="bg-[#dbcfcb] min-h-100 lg:min-h-170 rounded-lg grid grid-rows-11 p-4 lg:p-10" onSubmit={manejarEnvio}>
                         <div className="row-span-2 pt-2">
-                            <label className="font-bold">Nombre</label>
-                            <input type="text" placeholder="Tu nombre"
+                            <label htmlFor="nombre" className="font-bold">Nombre</label>
+                            <input id="nombre" name="nombre" type="text" placeholder="Tu nombre"
                                 className="bg-white w-full mt-2 p-3 rounded-lg focus:outline-none focus:ring-1 focus:ring-black"/>
                         </div>
                         <div className="row-span-2 pt-2">
-                            <label className="font-bold">Correo electrónico</label>
-                            <input type="text" placeholder="Tu correo electrónico"
+                            <label htmlFor="correo" className="font-bold">Correo electrónico</label>
+                            <input id="correo" name="correo" type="text" placeholder="Tu correo electrónico"
                                 className="bg-white w-full mt-2 p-3 rounded-lg focus:outline-none focus:ring-1 focus:ring-black"/>
                         </div>
                         <div className="row-span-2 pt-2">
-                            <label className="font-bold">Asunto</label>
-                            <input type="text" placeholder="Asunto del mensaje"
+                            <label htmlFor="asunto" className="font-bold">Asunto</label>
+                            <input id="asunto" name="asunto" type="text" placeholder="Asunto del mensaje"
                                 className="bg-white w-full mt-2 p-3 rounded-lg focus:outline-none focus:ring-1 focus:ring-black"/>
                         </div>
                         <div className="row-span-4 pt-2">
-                            <label className=" block font-bold">Mensaje</label>
-                            <textarea placeholder="Escribe aquí tu mensaje" rows={6} 
+                            <label htmlFor="mensaje" className=" block font-bold">Mensaje</label>
+                            <textarea id="mensaje" name="mensaje" placeholder="Escribe aquí tu mensaje" rows={6} 
                                 className="bg-white w-full mt-2 rounded-lg p-3"></textarea>
                         </div>
                         <div className="pt-4">
